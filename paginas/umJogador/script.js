47a9f0c4a2fda4daab287f42c032adb4e985b893
junior <juniormathiias@gmail.com>
2023-03-26 22:39:57 -0300

    adicionando as funcoes para mostrar dado e valores

--- paginas/umJogador/script.js
+++ paginas/umJogador/script.js
@@ -1,17 +1,44 @@
-function button(){
-    var number = document.getElementById('insertNumber')
+var number = document.getElementById('insertNumber')
+
+function girar(){
     var randomNumber = Math.floor(Math.random()*6) + 1
-    var img = document.getElementById('imgWainting')
+    switch(randomNumber){
+        case 1:
+            document.getElementById('face').src = "/img/dados/dice1.png";
+            break;
+        case 2:
+            document.getElementById('face').src = "/img/dados/dice2.png";
+            break;
+        case 3:
+            document.getElementById('face').src = "/img/dados/dice3.png";
+            break;
+        case 4:
+            document.getElementById('face').src = "/img/dados/dice4.png";
+            break;
+        case 5:
+            document.getElementById('face').src = "/img/dados/dice5.png";
+            break;
+        case 6:
+            document.getElementById('face').src = "/img/dados/dice6.png";
+            break;
+        default:
+    }
     if(number.value.length == 0){
         alert('Choose the number')
         document.getElementById('result').textContent = `Choose The Number `
     } else  if(randomNumber == number.value) {
         alert('You are Right ')
-        document.getElementById('result').textContent = `How did you know ? I was thinking about the number ${randomNumber}`
-        img.src = 'surprise.png'
     } else{
-        alert('You are wrong ')
+        alert('You are wrong  ' + randomNumber)
         document.getElementById('result').textContent = `Come on try again, I was thinking about the number ${randomNumber}`
-        img.src ='happy.png'
     }
 }
+
+function apostar(){
+    let randomNumber = girar();
+    
+   
+}
+
+
+
