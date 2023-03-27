3d498a2e65f087fc8b85f0e76abef7df15a8b56f
junior <juniormathiias@gmail.com>
2023-03-26 21:24:09 -0300

    começando o primeiro jogador jogando

---
+++ paginas/umJogador/script.js
@@ -0,0 +1,16 @@
+function button(){
+    var number = document.getElementById('insertNumber')
+    var randomNumber = Math.floor(Math.random()*6) + 1
+    var img = document.getElementById('imgWainting')
+    if(number.value.length == 0){
+        alert('Choose the number')
+        document.getElementById('result').textContent = `Choose The Number `
+    } else  if(randomNumber == number.value) {
+        alert('You are Right ')
+        document.getElementById('result').textContent = `How did you know ? I was thinking about the number ${randomNumber}`
+        img.src = 'surprise.png'
+    } else{
+        document.getElementById('result').textContent = `Come on try again, I was thinking about the number ${randomNumber}`
+        img.src ='happy.png'
+    }
+}
