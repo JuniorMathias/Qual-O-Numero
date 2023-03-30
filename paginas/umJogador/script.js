2a63cc6e1f2f86dfe1639e4a4678a5967d41ab1d
junior <juniormathiias@gmail.com>
2023-03-30 00:33:50 -0300

    alguns ajustes pra diminuir o codigo

--- paginas/umJogador/script.js
+++ paginas/umJogador/script.js
@@ -1,5 +1,9 @@
-var number = document.getElementById('insertNumber')
 const select1 =document.getElementById("select01");
+
+let botaoAposta = document.getElementById("apostar");
+
+let dado = document.getElementById('face');
+var number = document.getElementById('insertNumber')
 
 function valor(){
 
@@ -26,7 +30,6 @@ function valor(){
 function apostar(){
     
     if(number.value.length != 0){
-        document.getElementById("insertNumber").disabled = true;
         document.getElementById("girar").disabled = false;
     }else{
         document.getElementById('result').textContent = `Choose The Number `
@@ -38,22 +41,22 @@ function girar(){
     var randomNumber = Math.floor(Math.random()*6) + 1
     switch(randomNumber){
         case 1:
-            document.getElementById('face').src = "/img/dados/dice1.png";
+            dado.src = "/img/dados/dice1.png";
             break;
         case 2:
-            document.getElementById('face').src = "/img/dados/dice2.png";
+            dado.src = "/img/dados/dice2.png";
             break;
         case 3:
-            document.getElementById('face').src = "/img/dados/dice3.png";
+            dado.src = "/img/dados/dice3.png";
             break;
         case 4:
-            document.getElementById('face').src = "/img/dados/dice4.png";
+            dado.src = "/img/dados/dice4.png";
             break;
         case 5:
-            document.getElementById('face').src = "/img/dados/dice5.png";
+            dado.src = "/img/dados/dice5.png";
             break;
         case 6:
-            document.getElementById('face').src = "/img/dados/dice6.png";
+            dado.src = "/img/dados/dice6.png";
             break;
         default:
     }
@@ -66,12 +69,14 @@ function girar(){
     
             document.getElementById('result').textContent = `acertou ${randomNumber}`
             document.getElementById("insertNumber").disabled = false;
+            botaoAposta.disabled = true;
             document.getElementById("girar").disabled = true;
     
         } else{
     
             document.getElementById('result').textContent = `errou ${randomNumber}`
             document.getElementById("insertNumber").disabled = false;
+            botaoAposta.disabled = true;
             document.getElementById("girar").disabled = true;
     
             number.value = '';
